1533bb24a8232f24e5d0b471c6a15708fd722ccb
Joao Vitor <diassenai56@gmail.com>
2025-10-04 09:44:13 -0300

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import model.Cachorro;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Cachorro cachorro = new Cachorro("Lex", "Kangal",4,"John");
+
+        cachorro.latir();
+        cachorro.mostrarDados();
+
+    }
+}
---
+++ src/main/java/model/Cachorro.java
@@ -0,0 +1,65 @@
+package model;
+
+public class Cachorro {
+
+    private String nome;
+    private String raca;
+    private int idade;
+    private String dono;
+
+    public Cachorro(String nome, String raca, int idade, String dono) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.dono = dono;
+    }
+
+    public void latir(){
+        System.out.printf("%s está latindo: Au Au !!!\n", getNome());
+    }
+
+    public void mostrarDados(){
+
+        System.out.println("================");
+        System.out.println("Nome do cachorro: " + getNome());
+        System.out.println("Idade do cachorro: " + getIdade());
+        System.out.println("Raça do cachorro: " + getRaca());
+        System.out.println("Nome do dono do cachorro: " + getDono());
+        System.out.println("================");
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+}
+
